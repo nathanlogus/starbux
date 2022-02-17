80161745555e73bb38f1e6d8a8e4b0db406d540b
Nathan Ribeiro <nathan@DESKTOP-CHQIEJM.localdomain>
2022-02-17 17:59:23 +0100

    Adding the Spring Initialzr to the project

---
+++ src/main/java/com/starbux/starbuxbackend/StarbuxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.starbux.starbuxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbuxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbuxBackendApplication.class, args);
+	}
+
+}
