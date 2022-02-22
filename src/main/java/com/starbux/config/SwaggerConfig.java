d39519dc55cdaf39ec16475e27985825412b0210
Nathan Ribeiro <nathanpirral@gmail.com>
2022-02-22 22:46:42 +0100

    Fixing Swagger

--- src/main/java/com/starbux/config/SwaggerConfig.java
+++ src/main/java/com/starbux/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.starbux.starbuxbackend"))
+                .apis(RequestHandlerSelectors.basePackage("com.starbux"))
                 .paths(PathSelectors.any())
                 .build();
     }
